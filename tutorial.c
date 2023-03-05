e52bc04c6a2ab32120ec20e20cb6b003fe6a1782
YongHyun-Yi <dldydgus1995@naver.com>
2023-03-06 04:28:26 +0900

    간단 기능구현...

--- tutorial.c
+++ tutorial.c
@@ -2,12 +2,45 @@
 #include "so_long.h"
 #include "minilibx-linux/mlx.h"
 
+typedef struct s_pos
+{
+	void *mlx_ptr;
+	void *win_ptr;
+	int	x;
+	int	y;
+}	t_pos;
+
+int deal_key(int key, void *param)
+{
+	t_pos *player_pos;
+
+	player_pos = (t_pos *)param;
+	if (key == 65362) // up
+		player_pos->y--;
+	else if (key == 65364) // down
+		player_pos->y++;
+	else if (key == 65361) // left
+		player_pos->x--;
+	else if (key == 65363) // right
+		player_pos->x++;
+	mlx_pixel_put(player_pos->mlx_ptr, player_pos->win_ptr, player_pos->x, player_pos->y, 0xFFFFFF);
+	// ft_printf("key code: %d\n", key);
+	return (0);
+}
+
 int main()
 {
 	void *mlx_ptr;
 	void *win_ptr;
+	t_pos player_pos;
 
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
+	player_pos.mlx_ptr = mlx_ptr;
+	player_pos.win_ptr = win_ptr;
+	player_pos.x = 250;
+	player_pos.y = 250;
+	mlx_pixel_put(mlx_ptr, win_ptr, player_pos.x, player_pos.y, 0xFFFFFF);
+	mlx_key_hook(win_ptr, deal_key, &player_pos);
 	mlx_loop(mlx_ptr);
 }

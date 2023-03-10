647b0ede9ff87856e2bcd1e56b452e30eba9512e
YongHyun-Yi <dldydgus1995@naver.com>
2023-03-11 05:21:08 +0900

    sprite 준비, map 준비, 간단하게 과제구현만 하기... parsing 구현 전 준비

---
+++ src/so_long.c
@@ -0,0 +1,17 @@
+
+#include "so_long.h"
+
+int main()
+{
+	/*
+
+	gnl로 파일 읽어오기
+	문자열에 저장
+	strlen으로 문자열 길이 비교로 사각형인지 체크
+	exit, collectible, player 의 개수를 세기
+	에러 처리
+
+	char **에 이중배열로 저장해서 인덱스로 접근하고싶음...
+	
+	*/
+}
